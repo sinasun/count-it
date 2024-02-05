a62af96727f20e83c958a38692d572f5c705375a
sinasun <sinakhodaveisi@gmail.com>
2024-02-04 22:39:51 -0500

    :sparkles: added clap to the project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(
+    author = "sinasun",
+    version,
+    about = "A simple program to count characters, words and lines in folder and files"
+)]
+
+struct Arguments {
+    path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+
+    println!("{}", args.path);
 }
