c6e05bfd8c1bd2fcfa48279da952b63a0e96921e
sinasun <sinakhodaveisi@gmail.com>
2024-02-05 19:34:26 -0500

    :sparkles: clap args updated

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
-use clap::Parser;
+use clap::{ArgAction, Parser};
 
 #[derive(Parser)]
 #[clap(
     author = "sinasun",
-    version,
-    about = "A simple program to count characters, words and lines in folder and files"
+    version = "1.0",
+    about = "A simple program to count characters, words and lines in folders and files"
 )]
 
 struct Arguments {
+    #[clap(help = "Path for a file or a folder to count")]
     path: String,
+
+    #[clap(short, long, help = "Add characters counter", action=ArgAction::SetTrue)]
+    characters: bool,
+
+    #[clap(short, long, help = "Add words counter", action=ArgAction::SetTrue)]
+    words: bool,
+
+    #[clap(short, long, help = "Add lines counter", action=ArgAction::SetTrue)]
+    lines: bool,
+
+    #[clap(
+        short,
+        long,
+        help = "Depth of subfolders to print, if not provided print all"
+    )]
+    print_depth: Option<u16>,
+
+    #[clap(
+        short,
+        long,
+        help = "Max depth of sub folders to perform counting. If not provided, stops when no more sub folder is remaining"
+    )]
+    max_depth: Option<u16>,
 }
 
 fn main() {
